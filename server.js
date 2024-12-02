9bab2abe9cb7d3dc8722ac6394dc968a384eb016
ChandanaVaidya <chandana.vaidya@niveussolutions.com>
2024-12-02 12:27:30 +0530

    description

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+// STEP 1: Create an express server
+
+// STEP 2: Create a GET api that returns query param, path param and return both values with info as json
+
+// STEP 3: Create a POST api which acccepts a JSON data. Return an array of data.
+
 const express = require('express');
 const app = express();
-const port = 8000;
+const PORT = 8000;
 
 // Parse json middleware
 app.use(express.json());
@@ -30,6 +36,6 @@ app.post('/data', (req, res) => {
 });
 
 // Starting server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
